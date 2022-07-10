733240cb4c0d4c6756c3cb677a2064c87c093600
illgetmine16 <illgetmine16@gmail.com>
2022-07-10 19:22:45 -0400

    58:34

--- base/views.py
+++ base/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+rooms = [
+    {'id': 1, 'name': 'Lets learn Python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend Developers'},
+]
+
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request):
+    return render(request, 'base/room.html')
